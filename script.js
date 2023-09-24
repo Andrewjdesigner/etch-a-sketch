ba0b0d07dd4d722a1ad08db3effadaf2ec64921b
Andrewjdesigner <andrew.johnson0993@gmail.com>
2023-09-24 17:55:10 -0400

    Create 16 x 16 square grid responsive to container

---
+++ script.js
@@ -0,0 +1,21 @@
+function createGrid() {
+
+    const gridContainer = document.querySelector(".container");
+    
+    for (i = 0; i < 16; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("row");
+        gridContainer.appendChild(gridRow);
+    }
+
+    const gridRows = document.querySelectorAll(".row");
+    gridRows.forEach((row) => {
+        for(j = 0; j < 16; j++) {
+            const gridPosition = document.createElement("div");
+            gridPosition.classList.add("pixel");
+            row.appendChild(gridPosition);
+        }
+    });
+}
+
+createGrid();
